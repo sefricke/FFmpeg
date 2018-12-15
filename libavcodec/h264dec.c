5687dfd3f79c15fbb01378b69e84630072d459cd
Jernej Skrabec <jernej.skrabec@siol.net>
2018-12-15 22:32:16 +0100

    Add V4L2 request API h264 hwaccel
    Signed-off-by: Jernej Skrabec <jernej.skrabec@siol.net>
    Signed-off-by: Jonas Karlman <jonas@kwiboo.se>

--- libavcodec/h264dec.c
+++ libavcodec/h264dec.c
@@ -1100,6 +1100,9 @@ const FFCodec ff_h264_decoder = {
 #if CONFIG_H264_VIDEOTOOLBOX_HWACCEL
                                HWACCEL_VIDEOTOOLBOX(h264),
 #endif
+#if CONFIG_H264_V4L2REQUEST_HWACCEL
+                               HWACCEL_V4L2REQUEST(h264),
+#endif
                                NULL
                            },
     .caps_internal         = FF_CODEC_CAP_EXPORTS_CROPPING |
